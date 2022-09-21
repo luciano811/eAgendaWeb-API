61a76bdfdf8677b841022bb2289f915f04e14027
Luciano Dalmollin <xlucianox811@gmail.com>
2022-09-21 14:36:47 -0300

    Add project files.

---
+++ eAgenda.Webapi/Controllers/TarefasController.cs
@@ -0,0 +1,31 @@
+﻿using eAgenda.Aplicacao.ModuloTarefa;
+using eAgenda.Dominio.ModuloTarefa;
+using eAgenda.Infra.Configs;
+using eAgenda.Infra.Orm;
+using eAgenda.Infra.Orm.ModuloTarefa;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace eAgenda.Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        [HttpGet]        
+        public List<Tarefa> SelecionarTodos()
+        {
+            var config = new ConfiguracaoAplicacaoeAgenda();
+            var eAgendaDBContext = new eAgendaDbContext(config.ConnectionStrings);
+            var repositorioTarefa = new RepositorioTarefaOrm(eAgendaDBContext);
+            var servicoTarefa = new ServicoTarefa(repositorioTarefa, eAgendaDBContext);
+            var tarefaResult = servicoTarefa.SelecionarTodos(StatusTarefaEnum.Todos);
+            
+            if (tarefaResult.IsSuccess)
+                return tarefaResult.Value;
+
+            return null;
+        }
+    }
+}
---
+++ eAgenda.Webapi/ViewModels/ListarTarefaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace eAgenda.Webapi.ViewModels
+{
+    public class ListarTarefaViewModel
+    {
+        public string Titulo { get; set; }
+        public string Prioridade { get; set; }
+        public string Situacao { get; set; }
+
+    }
+}
